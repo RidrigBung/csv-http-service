4c8734735743b76715809803dd7199e76ee89898
Andrew Babanin <baban1n.and@yandex.ru>
2023-09-07 23:29:50 +0300

    can get csv files list now

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from functions import title, UPLOAD_FOLDER, is_allowed_file
-from static.constants import title, UPLOAD_FOLDER
+from functions import is_allowed_file, check_folder, get_csv_list
+from constants import title, UPLOAD_FOLDER
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Ограничение максимального размера файла в 64 мегабайт
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', title=title)
+    return render_template("index.html", title=title)
 
 
 @app.route("/csv-management/load-file", methods=["GET", "POST"])
@@ -19,12 +21,19 @@ def load_file():
         file = request.files["file"]
         filename = secure_filename(file.filename)
         if file and is_allowed_file(filename):
+            check_folder()
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             return render_template("successed_file_load.html", title=title, filename=filename)
         else:
             return render_template("failed_file_load.html", title=title, filename=filename)
     # request.method == "GET"
     return render_template("load_file.html", title=title)
+
+
+@app.route("/csv-management/get_files_list")
+def get_files_list():
+    data = get_csv_list()
+    return render_template("get_files_list.html", title=title, data=data)
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,6 +1,26 @@
-from static.constants import ALLOWED_EXTENSIONS
+import os
+import csv
+from typing import List, Union
+from constants import ALLOWED_EXTENSIONS, UPLOAD_FOLDER
 
 
 def is_allowed_file(filename: str) -> bool:
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def check_folder():
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.mkdir(UPLOAD_FOLDER)
+
+
+def get_csv_list() -> List[List[Union[str, List[str]]]]:
+    check_folder()
+    headers: List[List] = []
+    files = os.listdir(UPLOAD_FOLDER)
+    for filename in files:
+        with open(os.path.join(UPLOAD_FOLDER, filename), "r") as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            headers.append(list(next(reader)))
+    files = [filename.split('.')[0] for filename in files]
+    return zip(files, headers)
