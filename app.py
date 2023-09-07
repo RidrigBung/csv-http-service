7391021be086f3f4044e5195198474bdb5a85a0f
Andrew Babanin <baban1n.and@yandex.ru>
2023-09-08 00:44:58 +0300

    get fila data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from functions import is_allowed_file, check_folder, get_csv_list
+from functions import is_allowed_file, check_folder, get_csv_list, get_csv_file_data
 from constants import title, UPLOAD_FOLDER
 
 app = Flask(__name__)
@@ -34,6 +34,36 @@ def load_file():
 def get_files_list():
     files = get_csv_list()
     return render_template("get_files_list.html", title=title, files=files)
+
+
+@app.route("/csv-management/get_file_data", methods=["GET", "POST"])
+def get_filename():
+    if request.method == "POST":
+        filename_text = request.form["filename_text"]
+        filename_select = request.form["filename_select"]
+        if not filename_text:
+            if not filename_select:
+                return render_template("bad_filename.html", title=title, error="empty input")
+            else:
+                filename = filename_select
+        else:
+            filename = filename_text
+        filename += ".csv"
+        check_folder()
+        files = os.listdir(UPLOAD_FOLDER)
+        if filename not in files:
+            return render_template("bad_filename.html", title=title, error="no such file")
+        return redirect(url_for("get_file_data", filename=filename))
+    # request.method == "GET"
+    files = os.listdir(UPLOAD_FOLDER)
+    options = [filename.split('.')[0] for filename in files]
+    return render_template("get_filename.html", title=title, options=options)
+
+
+@app.route("/csv-management/get_file_data/<filename>")
+def get_file_data(filename):
+    data = get_csv_file_data(filename)
+    return render_template("get_file_data.html", title=title, filename=filename, data=data)
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -15,8 +15,8 @@ def check_folder():
 
 
 def get_csv_list() -> List[List[Union[str, List[str]]]]:
-    check_folder()
     headers: List[List] = []
+    check_folder()
     files = os.listdir(UPLOAD_FOLDER)
     for filename in files:
         with open(os.path.join(UPLOAD_FOLDER, filename), "r") as csvfile:
@@ -24,3 +24,11 @@ def get_csv_list() -> List[List[Union[str, List[str]]]]:
             headers.append(list(next(reader)))
     files = [filename.split('.')[0] for filename in files]
     return zip(files, headers)
+
+
+def get_csv_file_data(filename: str) -> List[List[str]]:
+    data = []
+    with open(os.path.join(UPLOAD_FOLDER, filename), encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        data = list(reader)
+    return data
